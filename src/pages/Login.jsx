7d2564df9286acd619da1443632725c13815059c
aanzapi <cuyaan08@gmail.com>
2025-09-11 10:15:34 +0700

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import { Link } from "react-router-dom";
 function Login() {
   const [emailOrUser, setEmailOrUser] = useState("");
   const [password, setPassword] = useState("");
